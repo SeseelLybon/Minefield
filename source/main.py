41f8b8a2d39013007e20d138660268efab5f58c3
Seseel Lybon <Seseellybon@gmail.com>
2019-04-23 16:02:28 +0200

    Minor progress
    Fixed typo
    Forced screen draw every second

--- source/main.py
+++ source/main.py
@@ -3,22 +3,11 @@ import pyglet
 from pyglet.window import key
 from pyglet.window import mouse
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
-import random
-from position import Position
-import os
-from chunkmanager import ChunkManager
-from scoremanager import ScoreManager
 from savemanager import SaveManager
 from configmanager import ConfigManager
-from statemanager import ScreenStates
-from statemanager import StateManager
-
-resources_folder = "resources/"
-
-logging.critical("Booting...")
-logging.info("Root... %s", __file__ )
+import logging
+logging.basicConfig(level=logging.DEBUG)
+import os
 
 if not os.path.exists("resources\\config.json"):
     ConfigManager.createdefaultconfig()
@@ -26,22 +15,22 @@ else:
     logging.debug("Config already exists")
     ConfigManager.loadexistingconfig()
 
+
+import random
+from position import Position
+from chunkmanager import ChunkManager
+from statemanager import state_dict
+from statemanager import MineField
+
+resources_folder = "resources/"
+
+logging.critical("Booting...")
+logging.info("Root... %s", __file__ )
+
 windowsize = ConfigManager.config_dict.get("window_size")
 
 #fps_display = pyglet.clock.ClockDisplay()
 window = pyglet.window.Window(width=windowsize[0],height=windowsize[1])
-
-score_label = pyglet.text.Label('score: ' + str(0),
-                                font_name='Times New Roman',
-                                font_size=12,
-                                x=50, y=window.height-50,
-                                anchor_x='left', anchor_y='center')
-
-clearedtiles_label = pyglet.text.Label('Tiles cleared: '+str(0),
-                        font_name='Times New Roman',
-                        font_size=12,
-                        x=50, y=window.height-75,
-                        anchor_x='left', anchor_y='center')
 
 chunk_dict = ChunkManager.chunk_dict
 
@@ -67,58 +56,63 @@ else:
         print("seed is %s", seed)
         ChunkManager.updategenchunks(offset, (window.width, window.height))
 
+Screenstate = "MineField"
+
 @window.event
 def on_draw():
     global offset
     global window
     window.clear()
 
-    if StateManager.Screenstate == ScreenStates.MineField
-        score_label.text = 'score: ' + str(ScoreManager.getscore())
-        clearedtiles_label.text = 'Tiles cleared: ' + str(ScoreManager.getclearedtiles())
+    if Screenstate == "MineField":
+        state_dict[Screenstate].draw(offset, window)
+    elif Screenstate == "MainMenu":
+        state_dict[Screenstate].draw(offset, window)
 
-
-        #ChunkManager.updatesprites(offset)
-
-        #Screenspace occlusion of the chunks
-        ChunkManager.screenspaceocclude_drawchunks(offset, (window.width, window.height))
-
-
-        score_label.draw()
-        clearedtiles_label.draw()
-        #fps_display.draw()
-        ChunkManager.updategenchunks(offset, (window.width, window.height))
-    elif StateManager.Screenstate == ScreenStates.MainScreen:
-        logging.critical("Screenstate MainScreen is empty!!!")
 
 @window.event
 def on_key_press(symbol, modifiers):
+    global Screenstate
+
     if symbol == key.UP:
         print("Hello world")
+    if Screenstate == "MineField":
+        if symbol == key.BACKSPACE:
+            Screenstate = "MainMenu"
+    elif Screenstate == "MainMenu":
+        if symbol == key.BACKSPACE:
+            Screenstate = "MineField"
 
 
 @window.event
 def on_mouse_release(x, y, button, modifiers):
-    global offset
 
-    x = x-offset[0]
-    y = y-offset[1]
+    if Screenstate == "MineField":
+        global offset
 
-    tilepos = Position.pixtotile((x,y))
-    chunkpos = Position.pixtochunk((x,y))
+        x = x-offset[0]
+        y = y-offset[1]
 
-    if button == mouse.LEFT:
-        logging.debug('Clicked at (%d, %d), %s, %s, %s', x, y, chunkpos, tilepos, offset)
-        #print('Clicked at', x, y, chunkpos, tilepos)
-        chunk = chunk_dict.get(chunkpos, None)
-        if chunk:
-            chunk.activatetile(tilepos=tilepos)
-    if button == mouse.RIGHT:
-        logging.debug('Clicked at (%d, %d), %s, %s, %s', x, y, chunkpos, tilepos, offset)
-        #print('Clicked at', x, y, chunkpos, tilepos)
-        chunk = chunk_dict.get(chunkpos, None)
-        if chunk:
-            chunk.flagtile(tilepos)
+        tilepos = Position.pixtotile((x,y))
+        chunkpos = Position.pixtochunk((x,y))
+
+        if button == mouse.LEFT:
+            logging.debug('Clicked at (%d, %d), %s, %s, %s', x, y, chunkpos, tilepos, offset)
+            #print('Clicked at', x, y, chunkpos, tilepos)
+            chunk = chunk_dict.get(chunkpos, None)
+            if chunk:
+                chunk.activatetile(tilepos=tilepos)
+        if button == mouse.RIGHT:
+            logging.debug('Clicked at (%d, %d), %s, %s, %s', x, y, chunkpos, tilepos, offset)
+            #print('Clicked at', x, y, chunkpos, tilepos)
+            chunk = chunk_dict.get(chunkpos, None)
+            if chunk:
+                chunk.flagtile(tilepos)
+    elif Screenstate == "MainMenu":
+        state_dict[Screenstate].getbuttonclicked(mouse_pos=(x,y), window=window)
+        #if buttonclicked == "New game":
+            #pass
+        pass
 
 
 @window.event
@@ -128,9 +122,10 @@ def on_mouse_scroll(x, y, scroll_x, scroll_y):
 @window.event
 def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
     global offset
-    if buttons == mouse.MIDDLE:
-        offset[0] += dx
-        offset[1] += dy
+    if Screenstate == "MineField":
+        if buttons == mouse.MIDDLE:
+            offset[0] += dx
+            offset[1] += dy
 
 
 logging.critical("Pyglet.run()")
@@ -138,6 +133,10 @@ logging.critical("Pyglet.run()")
 #import cProfile
 #cProfile.run('pyglet.app.run()')
 
+def falseupdate(dt):
+    pass
+
+pyglet.clock.schedule_interval_soft(falseupdate, 1//1)
 pyglet.app.run()
 
 logging.info("Saving map to file")
--- source/statemanager.py
+++ source/statemanager.py
@@ -2,12 +2,73 @@
 
 from enum import Enum
 from enum import auto
+import pyglet
+
+from chunkmanager import ChunkManager
+from scoremanager import ScoreManager
+from configmanager import ConfigManager
 
 
-class ScreenStates(Enum):
-    MainScreen = auto()
-    MineField = auto()
+class MineField:
+    window_size = ConfigManager.config_dict.get("window_size")
+    score_label = pyglet.text.Label('score: ' + str(0),
+                                        font_name='Times New Roman',
+                                        font_size=12,
+                                        x=50, y=window_size[1] - 50,
+                                        anchor_x='left', anchor_y='center')
+    clearedtiles_label = pyglet.text.Label('Tiles cleared: ' + str(0),
+                                       font_name='Times New Roman',
+                                       font_size=12,
+                                       x=50, y=window_size[1] - 75,
+                                       anchor_x='left', anchor_y='center')
+
+    @classmethod
+    def draw(cls, offset, window):
+        cls.score_label.text = 'score: ' + str(ScoreManager.getscore())
+        cls.clearedtiles_label.text = 'Tiles cleared: ' + str(ScoreManager.getclearedtiles())
+
+        ChunkManager.screenspaceocclude_drawchunks(offset, (window.width, window.height))
+        ChunkManager.updategenchunks(offset, (window.width, window.height))
+
+        cls.score_label.draw()
+        cls.clearedtiles_label.draw()
+
+class MainMenu:
+    window_size = ConfigManager.config_dict.get("window_size")
+
+    button_New_label = pyglet.text.Label("New game",
+                            font_name='Times New Roman',
+                            font_size=20,
+                            x=50, y=250,
+                            anchor_x='left', anchor_y='center')
+
+    button_Load_label = pyglet.text.Label("Load existing game",
+                            font_name='Times New Roman',
+                            font_size=20,
+                            x=window_size[0]//3, y=window_size[1] - 200,
+                            anchor_x='left', anchor_y='center')
+
+    button_Exit_label = pyglet.text.Label("Exit game",
+                            font_name='Times New Roman',
+                            font_size=20,
+                            x=window_size[0]//3, y=window_size[1] - 250,
+                            anchor_x='left', anchor_y='center')
+
+    @classmethod
+    def draw(cls, offset, window):
+        cls.button_New_label.draw()
+        cls.button_Load_label.draw()
+        cls.button_Exit_label.draw()
 
 
-class StateManager:
-    Screenstate = ScreenStates.MineField
+    @classmethod
+    def getbuttonclicked(cls, mouse_pos, window):
+        print("x Expect", cls.button_New_label.x, cls.button_New_label.x+200, "Got", mouse_pos[0])
+        print("y Expect", cls.button_New_label.y, cls.button_New_label.y+200, "Got", mouse_pos[1])
+
+        if cls.button_New_label.x > mouse_pos[0] > cls.button_New_label.x+200:
+            if cls.button_New_label.y > mouse_pos[1] > cls.button_New_label.y-50:
+                print("New game clicked"+str(mouse_pos))
+
+state_dict = {"MineField":MineField,
+              "MainMenu":MainMenu}
